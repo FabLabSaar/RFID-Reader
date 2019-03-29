ea6341d4f8ba1a7d0db6724c66c196a7c9fcbcfe
Florian Fromm <flrn.frmm@gmail.com>
2019-03-29 12:54:04 -0400

    Implemented read-poll
    If the reader reads the same uid again, it
    will not send the data again. This cooldown
    depends on an internal timer. The cooldown
    will be reseted, if a uid is reed again.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def read_card():
-    result = None
     (stat, tag_type) = card_reader.request(card_reader.REQIDL)
     if stat == card_reader.OK:
         (stat, raw_uid) = card_reader.anticoll()
@@ -11,37 +10,40 @@ def read_card():
                 if card_reader.auth(card_reader.AUTHENT1A, 8, key, raw_uid) == card_reader.OK:
                     payload = "{}".format(str(card_reader.read(8)))
                     card_reader.stop_crypto1()
-                    result = {
+                    return {
                         "TagType" : tag_type_str,
                         "UID" : uid_string,
                         "Payload" : payload
                     }
-    return result
+    return None
 
 def send_data(data):
-    print("Card Type:\t", data["TagType"])
-    print("Card Uid:\t", data["UID"])
-    print("Payload:\t", data["Payload"], '\n')
+    dataAsStr = "---\n"
+    dataAsStr += "Card Type: {0}\n".format(data["TagType"])
+    dataAsStr += "Card UID: {0}\n".format(data["UID"])
+    dataAsStr += "Payload: {0}\n".format(data["Payload"])
+    print(dataAsStr)
 
 def blink(count, duration_ms):
-    phases = 2 * count - 1
-    for i in range(phases):
+    phases = 2 * count
+    for _ in range(phases):
         led.value(0 if led.value() > 0 else 1)
         time.sleep_ms(int(duration_ms / phases))
 
 def run():
-    last_uid = None
+    last_uid = ""
     count = 0
     while True:
-        data = read_card()
-        if data is not None:
-            if data["UID"] is not last_uid:
-                send_data(data)
-                blink(3,1500)
-                last_uid = data["UID"]
-            count = 99
-        if count < 0 and last_uid is not None:
+        if count < 0 and last_uid != None:
             last_uid = None
         else:
             count -= 1
+        data = read_card()
+        if data != None:
+            if data["UID"] != last_uid:
+                send_data(data)
+                blink(5,2000)
+                last_uid = data["UID"]
+            count = 4
             
+run()
